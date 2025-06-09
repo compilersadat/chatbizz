2c33d270856da7b33b2e52f5b68fa9ddfd4a4704
compilersadat <sadcompiler@gmail.com>
2025-06-09 13:16:02 +0530

    api

--- routes/api.php
+++ routes/api.php
@@ -59,7 +59,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::post('/delvery-boy/login', [DriverAuthController::class, 'login']);
 Route::post('/user/login', [MerchantController::class, 'sendOtp']);
-Route::post('/user/login', [MerchantController::class, 'verifyOtp']);
+Route::post('/user/verifyOtp', [MerchantController::class, 'verifyOtp']);
 Route::get('/categories-with-merchants', [MerchantCategoryController::class, 'index']);
 Route::get('/merchant/{merchantId}/products', [ProductController::class, 'getProductsByMerchant']);
 Route::get('/products', [ProductController::class, 'getAllProducts']);
