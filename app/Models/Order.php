3ae04cf6fde9be726815587d280e431fbca5d762
compilersadat <sadcompiler@gmail.com>
2025-07-27 15:29:10 +0530

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -12,7 +12,7 @@ class Order extends Model
         'user_id', 'address_id', 'contact_name', 'contact_number',
         'sub_total', 'delivery_charges', 'platform_fee', 'total_amount',
         'merchant_transaction_id', 'status', 'delivery_partner_id', 'shop_id','razorpay_order_id', 'payment_gateway_id',
-        'pick_lat','pick_lng','drop_lng','drop_lat'
+        'pick_lat','pick_lng','drop_lng','drop_lat','completion_otp'
     ];
 
     public function items() { return $this->hasMany(OrderItem::class); }
