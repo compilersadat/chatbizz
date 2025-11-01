583f16f7198268657697a3b7056b2a5dc5ce5391
compilersadat <sadcompiler@gmail.com>
2025-11-01 11:40:46 +0530

    Update contact.blade.php

--- resources/views/policies/contact.blade.php
+++ resources/views/policies/contact.blade.php
@@ -5,8 +5,6 @@
 <ul class="list">
   <li>Email: <a href="mailto:support@chatbizz.in">support@chatbizz.in</a></li>
   <li>Address: Nanded, Maharashtra, India</li>
-  <li>(Optional) Phone: +91‑XXXXXXXXXX</li>
-  <li>(Optional) GSTIN: —</li>
 </ul>
 <p>For grievances or escalations, email <a href="mailto:support@chatbizz.in">support@chatbizz.in</a> with subject: <em>“Grievance – ChatBizz App”</em> and include your order ID and contact number.</p>
 @endsection
