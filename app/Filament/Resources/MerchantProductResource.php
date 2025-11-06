2a4e6872024be40180a70f1a1b443f3277d60414
compilersadat <sadcompiler@gmail.com>
2025-11-06 14:12:34 +0530

    Update MerchantProductResource.php

--- app/Filament/Resources/MerchantProductResource.php
+++ app/Filament/Resources/MerchantProductResource.php
@@ -65,7 +65,11 @@ class MerchantProductResource extends Resource
                 Forms\Components\TextInput::make('discount')
                     ->label('Discounted Price')
                     ->helperText('Leave empty to sell at original price.')
-                    ->numeric()->prefix('₹')->minValue(0)->rule('lte:price')->columnSpan(4),
+                    ->numeric()
+                    ->prefix('₹')
+                    ->minValue(0)
+                    ->rule('lte:data.price') // price input lives under the same data.* scope
+                    ->columnSpan(4),
 
                 Forms\Components\TextInput::make('stock')
                     ->numeric()->minValue(0)->required()->columnSpan(4),
